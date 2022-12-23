87708d39089c21bff3fd54692e920b2e492684aa
rayquaza7 <sarhaangulati737@gmail.com>
2022-12-23 15:17:17 -0800

    init

---
+++ scripts/content.js
@@ -0,0 +1,25 @@
+const input = document.querySelector(
+  "#__next > div > div.flex.h-full.flex-1.flex-col.md\\:pl-\\[260px\\] > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient > form > div > div.flex.flex-col.w-full.py-2.pl-3.flex-grow.md\\:py-3.md\\:pl-4.relative.border.border-black\\/10.bg-white.dark\\:border-gray-900\\/50.dark\\:text-white.dark\\:bg-gray-700.rounded-md.shadow-\\[0_0_10px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\].dark\\:shadow-\\[0_0_15px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\] > textarea"
+);
+
+const button = document.querySelector(
+  "#__next > div > div.flex.h-full.flex-1.flex-col.md\\:pl-\\[260px\\] > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient > form > div > div.flex.flex-col.w-full.py-2.pl-3.flex-grow.md\\:py-3.md\\:pl-4.relative.border.border-black\\/10.bg-white.dark\\:border-gray-900\\/50.dark\\:text-white.dark\\:bg-gray-700.rounded-md.shadow-\\[0_0_10px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\].dark\\:shadow-\\[0_0_15px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\] > button"
+);
+
+function promptEngineer() {
+  return `Answer the question as truthfully as possible, and if you're unsure of the answer, say "Sorry, I don't know".
+
+    You have access to a Python interpreter, so if you are not able to answer a question from memory, you can write a program that will answer the question.
+
+    Q. ${input.value}
+    A.`;
+}
+
+// only intercept if cmd + enter is pressed
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && e.metaKey) {
+    input.value = promptEngineer();
+    console.log("pressed cmd + enter");
+    console.log(input.value);
+  }
+});
