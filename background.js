e9158697cd2b1d822e70263630741a05210459ed
rayquaza7 <sarhaangulati737@gmail.com>
2022-12-24 22:43:43 -0800

    save checbox values

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// when a tab is updated, check if it is a chat.openai.com tab and send a message to the content script
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab.url.startsWith("https://chat.openai.com/chat")) {
     chrome.tabs.sendMessage(tabId, {
@@ -6,6 +7,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
+// on install add content script to all chat.openai.com tabs
 chrome.runtime.onInstalled.addListener(async () => {
   for (const cs of chrome.runtime.getManifest().content_scripts) {
     for (const tab of await chrome.tabs.query({ url: cs.matches })) {
